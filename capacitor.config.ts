12d691a2d6646b70eca36cebbb076002ef4e25c5
jianghua1 <719584530@qq.com>
2025-10-31 11:40:32 +0800

    删除app相关文件

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.example.app',
-  appName: 'stock-message-box2',
-  webDir: 'dist'
-};
-
-export default config;
